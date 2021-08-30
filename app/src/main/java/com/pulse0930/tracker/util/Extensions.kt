5f5c255e712dd163c0805c88055c6e4d617d0dad
pulse0930 <pallav2014@outlook.com>
2021-08-30 11:52:39 +0530

    weight and height fetched

--- app/src/main/java/com/pulse0930/tracker/util/Extensions.kt
+++ app/src/main/java/com/pulse0930/tracker/util/Extensions.kt
@@ -12,6 +12,7 @@ import com.google.android.gms.fitness.result.DataReadResponse
 import com.pulse0930.tracker.R
 import com.pulse0930.tracker.ui.calories.TAG
 import java.text.DateFormat
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 fun DataPoint.getStartTimeString(): String = DateFormat.getTimeInstance()
@@ -24,7 +25,37 @@ fun getVersion(packageManager: PackageManager, context: Context):String{
     val pInfo: PackageInfo = packageManager.getPackageInfo(context.getPackageName(), 0)
     return pInfo.versionName
 }
-
+fun getCalendarNow(): Calendar {
+    val now = Date()
+    val calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"))
+    calendar.time = now
+    return calendar
+}
+fun getCalendar(hour:Int,minute:Int,second:Int): Calendar {
+    val calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"))
+    calendar.set(Calendar.HOUR_OF_DAY, hour)
+    calendar.set(Calendar.MINUTE, minute)
+    calendar.set(Calendar.SECOND, second)
+    return calendar
+}
+fun IsBetweenTimeRange(calendar1:Calendar,calendar2:Calendar,calendar3:Calendar): Boolean {
+    if(calendar3.time.after(calendar1.time) && calendar3.time.before(calendar2.time)){
+        return true
+    }
+    return false
+}
+//    val calendar1 = getCalendar(t1[0].toInt(),t1[1].toInt(),t1[2].toInt())
+//    Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"))
+//    calendar1.set(Calendar.HOUR_OF_DAY, 8)
+//    calendar1.set(Calendar.MINUTE, 59)
+//    calendar1.set(Calendar.SECOND, 59)
+//    val calendar2 = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"))
+//    calendar2.set(Calendar.HOUR_OF_DAY, 23)
+//    calendar2.set(Calendar.MINUTE, 59)
+//    calendar2.set(Calendar.SECOND, 59)
+//    val now = Date()
+//    val calendar3 = Calendar.getInstance(TimeZone.getTimeZone("GMT+5:30"))
+//    calendar3.time = now
 fun printData(dataReadResult: DataReadResponse) {
     if (dataReadResult.buckets.isNotEmpty()) {
         Log.i(TAG, "Number of returned buckets of DataSets is: " + dataReadResult.buckets.size)
